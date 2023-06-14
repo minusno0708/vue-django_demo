02817ec10ba745b4e96904ca3dd30bddbba8d124
minusno0708 <naoyao1014@gmail.com>
2023-06-14 14:56:58 +0900

    client directory reset

--- client/src/common/api.service.js
+++
@@ -1,7 +0,0 @@
-import axios from "asios";
-
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "x-CSRFTOKEN";
-axios.defaults.withCredentials = true;
-
-export { axios };
